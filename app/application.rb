194f09f49993a290893a1824aa79ad8d11b189c7
danajackson2 <danajackson2@gmail.com>
2020-12-09 10:06:59 -0800

    good morning

---
+++ app/application.rb
@@ -0,0 +1,12 @@
+class Application
+    def call(env)
+        resp = Rack::Response.new
+
+        if Time.now.to_s.split[1].split(":")[0].to_i < 12
+            resp.write "Good Morning!"
+        else
+            resp.write "Good Afternoon!"
+        end
+        resp.finish
+    end
+end
